cac382972049e07ba30ba6d349884fdeae5f5622
edwards955 <getignorantfriday@gmail.com>
2023-09-18 15:05:20 -0400

    Add display for secret word

--- hangman.rb
+++ hangman.rb
@@ -2,3 +2,10 @@
 dictionary = File.readlines('dictionary.txt', chomp: true) if File.exist?('dictionary.txt')
 words = dictionary.select {|word| word.length.between?(5, 12)}
 secret_word = words.sample
+
+## Create Display for secret word and print it
+secret_word_display = Array.new(secret_word.length) {|i| "_"}
+puts secret_word_display.join(" ")
+
+guesses_left = 5
+

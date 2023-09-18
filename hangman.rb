19139e4b910b1495afcc20f20ac5f09e994d6664
edwards955 <getignorantfriday@gmail.com>
2023-09-18 15:45:58 -0400

    Modify displays for secret word and incorrect guesses

--- hangman.rb
+++ hangman.rb
@@ -2,31 +2,43 @@
 dictionary = File.readlines('dictionary.txt', chomp: true) if File.exist?('dictionary.txt')
 words = dictionary.select {|word| word.length.between?(5, 12)}
 secret_word = words.sample
+puts secret_word
 
 ## Create Display for secret word and print it
-secret_word_display = Array.new(secret_word.length) {|i| "_"}
-puts secret_word_display.join(" ")
+secret_word_display = Array.new(secret_word.length) { '_' }
 
 ## Create array to hold incorrect guesses
-incorrect_guesses = []
+incorrect = []
 
 guesses_left = 5
 
 def display(array)
-  puts array.join(' ')
+  puts "Secret Word: #{array.join(' ')}"
+  puts
 end
 
-puts 'Guess a letter:'
-guess = gets.chomp.downcase
-letters_found = (0...secret_word.length).find_all { |i| secret_word[i, 1] == guess }
-if letters_found.empty?
-  guesses_left =- 1
-  incorrect_guesses.push(guess)
-  puts "Incorrect. You have #{guesses_left} guesses left. Try again."
-else
-  letters_found.each do |i|
-    secret_word_display[i] = guess
-    puts 'Correct! Guess again.'
+def incorrect_display(array)
+  puts "Incorrect Letters: #{array.join(' ')}"
+  puts
+end
+
+until guesses_left.zero?
+  display(secret_word_display)
+  incorrect_display(incorrect)
+  puts 'Guess a letter:'
+  guess = gets.chomp.downcase
+  letters_found = (0...secret_word.length).find_all { |i| secret_word[i, 1] == guess }
+  if letters_found.empty?
+    guesses_left -= 1
+    incorrect.push(guess)
+    puts
+    puts "Incorrect. You have #{guesses_left} guesses left."
+  else
+    letters_found.each do |i|
+      secret_word_display[i] = guess
+    end
+    puts
+    puts 'Correct!'
   end
 end
 

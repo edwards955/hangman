b15cbfded39c5e1b5e9321c547fb46fa26e0cefa
edwards955 <getignorantfriday@gmail.com>
2023-09-18 16:17:49 -0400

    Move script into Hangman class

--- hangman.rb
+++ hangman.rb
@@ -1,45 +1,48 @@
-## Load Dictionary and select random word between 5 and 12 characters long
-dictionary = File.readlines('dictionary.txt', chomp: true) if File.exist?('dictionary.txt')
-words = dictionary.select {|word| word.length.between?(5, 12)}
-secret_word = words.sample
-puts secret_word
+class Hangman
+  attr_accessor :secret_word, :correct, :incorrect, :guesses_left
 
-## Create Display for secret word and print it
-secret_word_display = Array.new(secret_word.length) { '_' }
+  def initialize
+    dictionary = File.readlines('dictionary.txt', chomp: true) if File.exist?('dictionary.txt')
+    words = dictionary.select {|word| word.length.between?(5, 12)}
+    @secret_word = words.sample
+    @correct = Array.new(secret_word.length) { '_' }
+    @incorrect = []
+    @guesses_left = 5
+    puts secret_word
+  end
 
-## Create array to hold incorrect guesses
-incorrect = []
-
-guesses_left = 5
-
-def display(array)
-  puts "Secret Word: #{array.join(' ')}"
-  puts
-end
-
-def incorrect_display(array)
-  puts "Incorrect Letters: #{array.join(' ')}"
-  puts
-end
-
-until guesses_left.zero?
-  display(secret_word_display)
-  incorrect_display(incorrect)
-  puts 'Guess a letter:'
-  guess = gets.chomp.downcase
-  letters_found = (0...secret_word.length).find_all { |i| secret_word[i, 1] == guess }
-  if letters_found.empty?
-    guesses_left -= 1
-    incorrect.push(guess)
+  def display(array)
+    puts "Secret Word: #{array.join(' ')}"
     puts
-    puts "Incorrect. You have #{guesses_left} guesses left."
-  else
-    letters_found.each do |i|
-      secret_word_display[i] = guess
+  end
+
+  def incorrect_display(array)
+    puts "Incorrect Letters: #{array.join(' ')}"
+    puts
+  end
+
+  def play
+    until guesses_left.zero?
+      display(correct)
+      incorrect_display(incorrect)
+      puts 'Guess a letter:'
+      guess = gets.chomp.downcase
+      letters_found = (0...secret_word.length).find_all { |i| secret_word[i, 1] == guess }
+      if letters_found.empty?
+        self.guesses_left -= 1
+        incorrect.push(guess)
+        puts
+        puts "Incorrect. You have #{guesses_left} guesses left."
+      else
+        letters_found.each do |i|
+          correct[i] = guess
+        end
+        puts
+        puts 'Correct!'
+      end
     end
-    puts
-    puts 'Correct!'
   end
 end
 
-
+game = Hangman.new
+game.play

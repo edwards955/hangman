dcd7b532aae0842ab83bf9d9690cb8361148490d
edwards955 <getignorantfriday@gmail.com>
2023-09-18 15:28:15 -0400

    Add guessing logic

--- hangman.rb
+++ hangman.rb
@@ -7,5 +7,27 @@ secret_word = words.sample
 secret_word_display = Array.new(secret_word.length) {|i| "_"}
 puts secret_word_display.join(" ")
 
+## Create array to hold incorrect guesses
+incorrect_guesses = []
+
 guesses_left = 5
+
+def display(array)
+  puts array.join(' ')
+end
+
+puts 'Guess a letter:'
+guess = gets.chomp.downcase
+letters_found = (0...secret_word.length).find_all { |i| secret_word[i, 1] == guess }
+if letters_found.empty?
+  guesses_left =- 1
+  incorrect_guesses.push(guess)
+  puts "Incorrect. You have #{guesses_left} guesses left. Try again."
+else
+  letters_found.each do |i|
+    secret_word_display[i] = guess
+    puts 'Correct! Guess again.'
+  end
+end
+
 
